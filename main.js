3eea17d81a4c04c015a271c0f4822fef98ce40d5
KamilMarque <kamil.marque@gmail.com>
2019-07-25 16:21:24 +0200

    final

--- main.js
+++ main.js
@@ -2,22 +2,29 @@ $('document').ready(function() {
 
 	var select = document.getElementById("val");
 	var theme = document.getElementById("theme");
-	var bool = 0;
+	var avengersThemeTab = [
+			["Iron Man ", "Captain America ", "Thor ", "Loki ", "Jarvis ", "Oeil-de-faucon ", "Bruce Banner "],
+			["aime ", "deteste ", "apprecie ", "prefere ", "adore ", "ignore "],
+			["manger ", "vendre ", "laisser ", "attraper ", "ramasser ", "tenir "],
+			["le Joker ", "Superman ", "Batman ", "Spider-Man ", "Flash ", "Acquaman ", "Green Lantern "],
+			["quand ", "lorsqu'", "pendant qu'"],
+			["il est ", "il va ", "il sort "],
+			["aux toilettes.", "au parc", "chez sa mere.", "chez le dentiste.", "avec Deadpool au centre de dépistage."]
+		];
+	var noThemeTab = [
+			["Avec ", "Considérant ", "Où que nous mène ", "Eu égard à ", "Vu ", "En ce qui concerne ", "Dans le cas particulier de "],
+			["la restriction", "l'orientation ", "la crise ", "l'inertie ", "la difficulté ", "l'austérité ", "la dégradation "],
+			["présente, ", "actuelle, ", "générale, ", "induite, ", "conjoncturelle, ", "observée, ", "contextuelle, "],
+			["il faut ", "il faut de toute urgence ", "il est préférable d'", "il serait intéressant d'", "il ne faut pas négliger d'", "on ne peut se passer d'", "il est nécessaire d'"],
+			["étudier ", "examiner ", "favoriser ", "prendre en considération ", "anticiper ", "imaginer ", "uniformiser "],
+			["toutes les ", "chacune des ", "la majorité des ", "la simultanéité des ", "l'ensemble des ", "la somme des ", "la totalité des "],
+			["solutions ", "issues ", "problématiques ", "voies ", "alternatives ", "organisations matricielles ", "améliorations "],
+			["imaginables ", "possibles ", "s'offrant à nous ", "de bon sens ", "envisageables ", "éventuelles ", "réalisables "],
+			["à long terme.", "pour longtemps.", "à l'avenir.", "pour le futur.", "depuis longtemps.", "à court terme.", "rapidement."]
+		];
 
 	$('#generer').click(function() {
 		$('.citation').html(startCitations(select.selectedIndex + 1, theme.selectedIndex));
-		if (bool === 0) {
-			$('.citation').animate({
-        		left: '+=20px',
-    			})
-			bool += 1;
-		}
-		else {
-			$('.citation').animate({
-        		left: '-=20px',
-    			})
-			bool -= 1;
-		}
 	});
 
 	function getRandomArbitrary(min, max) {
@@ -37,64 +44,38 @@ $('document').ready(function() {
 		return (beginning)
 	}
 
-	function avengersThemeTab() {
-		var tab = [
-			["Iron Man ", "Captain America ", "Thor ", "Loki ", "Jarvis ", "Oeil-de-faucon ", "Bruce Banner "],
-			["aime ", "deteste ", "apprecie ", "prefere ", "adore ", "ignore "],
-			["manger ", "vendre ", "laisser ", "attraper ", "ramasser ", "tenir "],
-			["le Joker ", "Superman ", "Batman ", "Spider-Man ", "Flash ", "Acquaman ", "Green Lantern "],
-			["quand ", "lorsqu'", "pendant qu'"],
-			["il est ", "il va ", "il sort "],
-			["aux toilettes.", "au parc", "chez sa mere.", "chez le dentiste.", "avec Deadpool au centre de dépistage."]
-		]
-		return (tab);
-	}
-
-	function noThemeTab() {
-		var tab = [
-			["Avec ", "Considérant ", "Où que nous mène ", "Eu égard à ", "Vu ", "En ce qui concerne ", "Dans le cas particulier de "],
-			["la restriction", "l'orientation ", "la crise ", "l'inertie ", "la difficulté ", "l'austérité ", "la dégradation "],
-			["présente ", "actuelle ", "générale ", "induite ", "conjoncturelle ", "observée ", "contextuelle "],
-			["il faut ", "il faut de toute urgence ", "il est préférable d'", "il serait intéressant d'", "il ne faut pas négliger d'", "on ne peut se passer d'", "il est nécessaire d'"],
-			["étudier ", "examiner ", "favoriser ", "prendre en considération ", "anticiper ", "imaginer ", "uniformiser "],
-			["toutes les ", "chacune des ", "la majorité des ", "la simultanéité des ", "l'ensemble des ", "la somme des ", "la totalité des "],
-			["solutions ", "issues ", "problématiques ", "voies ", "alternatives ", "organisations matricielles ", "améliorations "],
-			["imaginables ", "possibles ", "s'offrant à nous ", "de bon sens ", "envisageables ", "éventuelles ", "réalisables "],
-			["à long terme.", "pour longtemps.", "à l'avenir.", "pour le futur.", "depuis longtemps.", "à court terme.", "rapidement."]
-		];
-		return (tab)
-	}
-
-	function getMaidTheme(line, theme) {
+	function getLengthTheme(theme) {
 		if (theme === 0)
-			var tab = noThemeTab();
+			return(noThemeTab.length);
 		else
-			var tab = avengersThemeTab();
-		if (line === -1)
-			return (tab.length);
+			return(avengersThemeTab.length);
+	}
+
+	function getPieceSentence(line, theme) {
+		if (theme === 0)
+			return (noThemeTab[line][parseInt(getRandomArbitrary(1, noThemeTab[line].length))]);
 		else
-			return (tab[line][parseInt(getRandomArbitrary(1, tab[line].length))]);
+			return (avengersThemeTab[line][parseInt(getRandomArbitrary(1, avengersThemeTab[line].length))]);
 	}
 
 	function getSentence(theme) {
-		var nb_line = getMaidTheme(-1);
-		var sentence = "";
-		var end = "";
+		var nb_line = getLengthTheme(theme);
+		var beginningOfSentence = "";
+		var endOfSentence = "";
 
 		for (var i = 0; i < nb_line; i++) {
-			console.log(theme);
-			end = getMaidTheme(i, theme);
-			console.log(theme);
-			sentence = eOrApostrophe(sentence, end) + end;
+			endOfSentence = getPieceSentence(i, theme);
+			beginningOfSentence = eOrApostrophe(beginningOfSentence, endOfSentence) + endOfSentence;
 		}
-		return (sentence + "\n");
+		return (beginningOfSentence + "\n");
 	}
 
-	function startCitations(val, theme) {
-		var total = "";
-		for (var i = 0; i < val; i++)
-			total = total + getSentence(theme);
-		total = total.replace(/\n/g, "<br /><br />");
-		return (total);
+	function startCitations(nbCitation, theme) {
+		var sentence = "";
+
+		for (var i = 0; i < nbCitation; i++)
+			sentence = sentence + getSentence(theme);
+		sentence = sentence.replace(/\n/g, "<br /><br />");
+		return (sentence);
 	}
 });
